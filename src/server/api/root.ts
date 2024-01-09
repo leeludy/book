70c7b10bed537b2baa64f44b846145f1aea21002
sibylle <sibylle.p@protonmail.com>
2024-01-09 16:39:48 +0100

    draft prisma schema

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { exempleRouter } from "~/server/api/routers/expl";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  expl: exempleRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/expl.ts
+++ src/server/api/routers/expl.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const exempleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -11,13 +11,13 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
+  /* create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
+      return ctx.db.expl.create({
         data: {
           name: input.name,
         },
@@ -25,8 +25,8 @@ export const postRouter = createTRPCRouter({
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
+    return ctx.db.expl.findFirst({
       orderBy: { createdAt: "desc" },
     });
-  }),
+  }), */
 });
