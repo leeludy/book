91878dd629530af4f0b214b8ad2ea0c33a52a3f0
sibylle <sibylle.p@protonmail.com>
2024-02-05 11:12:15 +0100

    prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 100,
+  semi: false,
 };
 
 export default config;
